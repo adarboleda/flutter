fe8125ec42e84057e82a2c0196c483f81f5aac25
Abner Arboleda <93734334+adarboleda@users.noreply.github.com>
2025-06-19 17:36:50 -0500

    height and weight cards

--- imc_calculator/lib/screens/imc_home_screen.dart
+++ imc_calculator/lib/screens/imc_home_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:imc_calculator/components/gender_selector.dart';
+import 'package:imc_calculator/components/height_selectot.dart';
+import 'package:imc_calculator/components/number_selector.dart';
 
 class ImcHomeScreen extends StatefulWidget {
   const ImcHomeScreen({super.key});
@@ -9,8 +11,55 @@ class ImcHomeScreen extends StatefulWidget {
 }
 
 class _ImcHomeScreenState extends State<ImcHomeScreen> {
+  int selectedAge = 20;
+  int selectedWeight = 80;
   @override
   Widget build(BuildContext context) {
-    return Column(children: [GenderSelector()]);
+    return Column(
+      children: [
+        GenderSelector(),
+        HeightSelector(),
+        Padding(
+          padding: const EdgeInsets.all(16),
+          child: Row(
+            children: [
+              Expanded(
+                child: NumberSelector(
+                  title: "PESO",
+                  value: selectedWeight,
+                  onIncrement: () {
+                    setState(() {
+                      selectedWeight++;
+                    });
+                  },
+                  onDecrement: () {
+                    setState(() {
+                      selectedWeight--;
+                    });
+                  },
+                ),
+              ),
+              SizedBox(width: 16),
+              Expanded(
+                child: NumberSelector(
+                  title: "EDAD",
+                  value: selectedAge,
+                  onIncrement: () {
+                    setState(() {
+                      selectedAge++;
+                    });
+                  },
+                  onDecrement: () {
+                    setState(() {
+                      selectedAge--;
+                    });
+                  },
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
   }
 }
